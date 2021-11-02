c0bad033d7510355632240806a83062de6f1c3fb
pphaal <peter.phaal@gmail.com>
2021-11-02 09:50:01 -0700

    add deep linking

--- html/js/app.js
+++ html/js/app.js
@@ -13,20 +13,23 @@ $(function() {
   var lastControlsID = 0;
 
   function setNav(target) {
-    $('.navbar .nav-item a[data-target="'+target+'"]').parent().addClass('active').siblings().removeClass('active');
-    $('#'+target).show().siblings().hide();
+    $('.navbar .nav-item a[href="'+target+'"]').parent().addClass('active').siblings().removeClass('active');
+    $(target).show().siblings().hide();
+    window.sessionStorage.setItem('ddos_protect_nav',target);
+    window.history.replaceState(null,'',target);
   }
 
-  setNav(window.sessionStorage.getItem('ddos_protect_nav') || 'charts');
+  var hash = window.location.hash;
+  if(hash && $('.navbar .nav-item a[href="'+hash+'"]').length == 1) setNav(hash);
+  else setNav(window.sessionStorage.getItem('ddos_protect_nav') || $('.navbar .nav-item a').first().attr('href'));
 
   $('.navbar .nav-link').on('click', function(e) {
-    var selected = $(this).data('target');
+    var selected = $(this).attr('href');
     setNav(selected);
-    window.sessionStorage.setItem('ddos_protect_nav',selected);
-    if('charts' === selected) $.event.trigger({type:'updateChart'});
+    if('#charts' === selected) $.event.trigger({type:'updateChart'});
   });
 
-  $('a[href="#"]').on('click', function(e) {
+  $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
   });
 
